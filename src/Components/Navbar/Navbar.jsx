9728f900affbbd59cd753f2880296ea7798e9d89
Obinna Nwobodo <obinnanwobodo27@gmail.com>
2025-09-24 01:41:56 +0100

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+import Hero from "@/Components/Hero/Hero";
+import Navbar from "@/Components/Navbar/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div>
+
+<div>
+  <Navbar/>
+</div>
+
+
+<div>
+  <Hero/>
+</div>
+
+
+    </div>
+  );
+}
